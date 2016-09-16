4616d64135b37f3572b55b2579894d15fb293488
Nitzan Shwartz <nitzandesigns@gmail.com>
2016-09-16 18:15:00 +0300

    .

--- ShipManagement1/ShipManagement1/Date.cs
+++ ShipManagement1/ShipManagement1/Date.cs
@@ -3,21 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ShipManagement1
 {
-    class Date
+    public class Date
     {
-
         private int day;
         private int month;
         private int year;
-
         public Date(int day, int month, int year)
         {
             this.day = day;
             this.month = month;
             this.year = year;
+        }
+        public Date(Date d)
+        {
+            if (d == null)
+            {
+                this.day = 1;
+                this.month = 1;
+                this.year = 1111;
+            }
+            else
+            {
+                this.day = d.day;
+                this.month = d.month;
+                this.year = d.year;
+            }
         }
 
         #region Get and Set
@@ -26,19 +40,59 @@ namespace ShipManagement1
             get { return day; }
             set { day = value; }
         }
-
         public int Month
         {
             get { return month; }
             set { month = value; }
         }
-
         public int Year
         {
             get { return year; }
             set { year = value; }
         }
+        #endregion
 
+        #region Read and Write the XML file
+        public static void WriteDate(Date d, string elementName, XmlWriter writer)
+        {
+            writer.WriteStartElement(elementName);
+
+            writer.WriteElementString("day", d.day.ToString());
+            writer.WriteElementString("month", d.month.ToString());
+            writer.WriteElementString("year", d.year.ToString());
+
+            writer.WriteEndElement();
+
+        }
+
+       
+        public static Date ReadDate(XmlReader xmlReader)
+        {
+            int day = 0, month = 0, year = 0;
+            while (xmlReader.Read())
+            {
+                if (xmlReader.NodeType == XmlNodeType.Element)
+                {
+                    string element = xmlReader.Name;
+                    if (element.Equals("day"))
+                    {
+                        xmlReader.Read();
+                        day = int.Parse(xmlReader.Value);
+                    }
+                    else if (element.Equals("month"))
+                    {
+                        xmlReader.Read();
+                        month = int.Parse(xmlReader.Value);
+                    }
+                    else if (element.Equals("year"))
+                    {
+                        xmlReader.Read();
+                        year = int.Parse(xmlReader.Value);
+                    }
+                }
+            }
+            return (new Date(day, month, year));
+        }
         #endregion
     }
 }
--- ShipManagement1/ShipManagement1/Shipment.cs
+++ ShipManagement1/ShipManagement1/Shipment.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ShipManagement1
 {
-    class Shipment
+    public class Shipment
     {
-        private String id;
         private Address from;
         private Address to;
         private Date recieveDate;
         private Date deliverDate;
         private double sum;
-        private String currency;
+        private string currency;
         private bool payed;
-
-        public Shipment(String id, Address from, Address to, Date recieveDate, Date deliverDate, double sum, String currency, bool payed)
+        private string id;
+        public Shipment(string id, Address from, Address to, Date recieveDate, Date deliverDate, double sum,
+            string currency, bool payed)
         {
-            this.id = id;
             this.from = from;
             this.to = to;
             this.recieveDate = recieveDate;
@@ -27,14 +27,28 @@ namespace ShipManagement1
             this.sum = sum;
             this.currency = currency;
             this.payed = payed;
-
+            this.id = id;
         }
-
-        #region Get and Set
-        public string Id
+        public Shipment(Shipment s)
         {
-            get { return id; }
-            set { id = value; }
+            if (s != null)
+            {
+                this.from = new Address(s.from);
+                this.to = new Address(s.to);
+                this.recieveDate = new Date(s.recieveDate);
+                this.deliverDate = new Date(s.deliverDate);
+                this.sum = s.sum;
+                this.currency = s.currency;
+                this.payed = s.payed;
+                this.id = s.id;
+            }
+            else
+            {
+                this.from = new Address(null);
+                this.to = new Address(null);
+                this.recieveDate = new Date(null);
+                this.deliverDate = new Date(null);
+            }
         }
 
         public Address From
@@ -42,42 +56,123 @@ namespace ShipManagement1
             get { return from; }
             set { from = value; }
         }
-
         public Address To
         {
             get { return to; }
             set { to = value; }
         }
-
         public Date RecieveDate
         {
             get { return recieveDate; }
             set { recieveDate = value; }
         }
-
         public Date DeliverDate
         {
             get { return deliverDate; }
             set { deliverDate = value; }
         }
-
         public double Sum
         {
             get { return sum; }
             set { sum = value; }
         }
-
-        public String Currency
+        public string Currency
         {
             get { return currency; }
             set { currency = value; }
         }
-
         public bool Payed
         {
             get { return payed; }
             set { payed = value; }
         }
-        #endregion
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+        public static void WriteShipment(Shipment ship, XmlWriter writer)
+        {
+            writer.WriteStartElement("Shipment");
+
+            writer.WriteElementString("id", ship.ID.ToString());
+            if (ship.from != null)
+                Address.WriteAddress(ship.from, "from", writer);
+            if (ship.to != null)
+                Address.WriteAddress(ship.to, "to", writer);
+            if (ship.recieveDate != null)
+                Date.WriteDate(ship.recieveDate, "recieveDate", writer);
+            if (ship.deliverDate != null)
+                Date.WriteDate(ship.deliverDate, "deliverDate", writer);
+            writer.WriteElementString("sum", ship.Sum.ToString());
+            writer.WriteElementString("currency", ship.currency);
+            writer.WriteElementString("payed", ship.payed.ToString());
+
+            writer.WriteEndElement();
+
+        }
+
+        public static Shipment ReadShipment(XmlReader xmlReader)
+        {
+            Address from = null, to = null;
+            Date recieveDate = null, deliverDate = null;
+            double sum = 0;
+            bool payed = false;
+            string id = null;
+            String currency = "";
+            XmlReader subTree;
+
+            while (xmlReader.Read())
+            {
+                if (xmlReader.NodeType == XmlNodeType.Element)
+                {
+                    String element = xmlReader.Name;
+
+                    if (element.Equals("id"))
+                    {
+                        xmlReader.Read();
+                        id = xmlReader.Value;
+                    }
+                    else if (element.Equals("from"))
+                    {
+                        subTree = xmlReader.ReadSubtree();
+                        from = Address.ReadAddress(subTree);
+                    }
+                    else if (element.Equals("to"))
+                    {
+                        subTree = xmlReader.ReadSubtree();
+                        to = Address.ReadAddress(subTree);
+                    }
+                    else if (element.Equals("recieveDate"))
+                    {
+                        subTree = xmlReader.ReadSubtree();
+                        recieveDate = Date.ReadDate(subTree);
+                    }
+                    else if (element.Equals("deliverDate"))
+                    {
+                        subTree = xmlReader.ReadSubtree();
+                        deliverDate = Date.ReadDate(subTree);
+                    }
+                    else if (element.Equals("sum"))
+                    {
+                        xmlReader.Read();
+                        sum = Double.Parse(xmlReader.Value);
+                    }
+                    else if (element.Equals("payed"))
+                    {
+                        xmlReader.Read();
+                        payed = bool.Parse(xmlReader.Value);
+                    }
+                    else if (element.Equals("currency"))
+                    {
+                        xmlReader.Read();
+                        currency = xmlReader.Value;
+                    }
+                }
+            }
+            return new Shipment(id, from, to, recieveDate, deliverDate, sum, currency, payed);
+        }
     }
 }
